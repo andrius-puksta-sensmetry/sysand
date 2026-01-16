c6bee1f08c00bf2c375c8ca56a33c45335c99a25
Andrius Pukšta <andrius.puksta@sensmetry.com>
2026-01-16 14:14:55 +0200

    fix
    Signed-off-by: Andrius Pukšta <andrius.puksta@sensmetry.com>

--- .github/scripts/install-linux-dependencies.sh
+++ .github/scripts/install-linux-dependencies.sh
@@ -10,6 +10,7 @@
 # If we're running on rhel centos, install needed packages.
 if command -v yum &> /dev/null; then
     # yum update -y && yum install -y perl-core openssl openssl-devel pkgconfig libatomic
+    echo "centos"
 
     # # If we're running on i686 we need to symlink libatomic
     # # in order to build openssl with -latomic flag.
@@ -19,4 +20,5 @@ if command -v yum &> /dev/null; then
 else
     # If we're running on debian-based system.
     # apt-get update -y && apt-get install -y libssl-dev openssl pkg-config
+    echo "debian"
 fi
