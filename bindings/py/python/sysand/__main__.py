9201194b2587a8fdc57c94e2cab3e10678af24fc
Andrius Pukšta <andrius.puksta@sensmetry.com>
2026-01-21 15:01:33 +0200

    fix
    Signed-off-by: Andrius Pukšta <andrius.puksta@sensmetry.com>

--- bindings/py/python/sysand/__main__.py
+++ bindings/py/python/sysand/__main__.py
@@ -1,10 +1,14 @@
+# SPDX-FileCopyrightText: © 2025 Sysand contributors <opensource@sensmetry.com>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
 import sys
 
-from sysand._sysand_core import _run_cli as run_cli_impl
+import sysand._sysand_core as sysand_rs  # type: ignore
 
 
 def main() -> int:
-    is_success = run_cli_impl(["sysand"] + sys.argv[1:])
+    is_success = sysand_rs._run_cli(["sysand"] + sys.argv[1:])
     return 0 if is_success else 1
 
 
